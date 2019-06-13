943eef8b324ae94b2e4a29bdd15cf37720ae375e
Wojciech Jaronski <wojciech.jaronski@impaqgroup.com>
2019-06-13 10:39:00 +0200

    Initial commit

---
+++ src/main/kotlin/net/ddns/jaronsky/studia/tiwpr/websockets/TiwprWebsocketsApplication.kt
@@ -0,0 +1,11 @@
+package net.ddns.jaronsky.studia.tiwpr.websockets
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TiwprWebsocketsApplication
+
+fun main(args: Array<String>) {
+    runApplication<TiwprWebsocketsApplication>(*args)
+}
---
+++ src/test/kotlin/net/ddns/jaronsky/studia/tiwpr/websockets/TiwprWebsocketsApplicationTests.kt
@@ -0,0 +1,16 @@
+package net.ddns.jaronsky.studia.tiwpr.websockets
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TiwprWebsocketsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
